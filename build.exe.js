18faa7e411f7eeceb164869e20bc00367a2fd884
空巷一人 <1530688385@qq.com>
2024-09-06 14:21:38 +0800

    删除test

--- build.exe.js
+++ build.exe.js
@@ -15,7 +15,9 @@ if (process.platform === 'win32') seaConfig.assets['web.config'] = './web.config
 
 writeFileSync('./sea-config.json', JSON.stringify(seaConfig, null, 2));
 
-execSync(`node --experimental-sea-config sea-config.json`);
+execSync(`node --experimental-sea-config sea-config.json`, {
+  stdio: 'inherit'
+});
 
 let runName =
   process.platform === 'win32'
@@ -32,17 +34,24 @@ rmSync(`./${runName}`, {
 cpSync(process.execPath, runName);
 
 if (process.platform === 'darwin') {
-  execSync(`codesign --remove-signature ${runName}`);
+  execSync(`codesign --remove-signature ${runName}`, {
+    stdio: 'inherit'
+  });
 }
 
 execSync(
   `npx postject ${runName} NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 ${
     process.platform === 'darwin' ? '--macho-segment-name NODE_SEA' : ''
-  }`
+  }`,
+  {
+    stdio: 'inherit'
+  }
 );
 
 if (process.platform === 'darwin') {
-  execSync(`codesign --sign - ${runName}`);
+  execSync(`codesign --sign - ${runName}`, {
+    stdio: 'inherit'
+  });
 }
 
 rmSync(`./dist`, {
